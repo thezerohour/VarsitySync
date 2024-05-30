b9605ca0a411ae072eff8b5df962c5dc9d010bfa
zzhengweii <ow.zhengwei@gmail.com>
2024-05-30 18:38:18 +0800

    replaced old VarsitySync file
    Havent put in the theme yet

---
+++ VarsitySync/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import AppNavigation from './navigation/appNavigation';
+
+function App() {
+  return (
+    <AppNavigation />
+  );
+}
+
+export default App;
---
+++ VarsitySync/navigation/appNavigation.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
