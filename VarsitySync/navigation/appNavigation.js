449786272bb27cfd2b9f0663c13cd1ca676cc760
zzhengweii <ow.zhengwei@gmail.com>
2024-07-08 13:56:24 +0800

    added tab navigation

--- VarsitySync/navigation/appNavigation.js
+++ VarsitySync/navigation/appNavigation.js
@@ -1,18 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { Image } from 'react-native';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import HomeScreen from '../screens/HomeScreen';
-import ToDoList from '../screens/ToDoList';
+import TaskScreen from '../screens/TaskScreen';
+import ScheduleScreen from '../screens/ScheduleScreen';
+import ProgressScreen from '../screens/ProgressScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { auth } from '../firebaseConfig';
 import { setUser } from '../redux/slices/user';
 import ForgetPasswordScreen from '../screens/ForgetPasswordScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TodoList from '../components/TodoList';
+
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function AppNavigation() {
   const {user} = useSelector(state => state.user);
@@ -28,8 +35,8 @@ export default function AppNavigation() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen options= {{headerShown: false, presentation: 'fullScreenModal'}} name="Login" component={LoginScreen} />
-          <Stack.Screen options= {{headerShown: false}} name="Home" component={HomeScreen} />
-          <Stack.Screen options= {{headerShown: false}} name="ToDoList" component={ToDoList} />
+          <Stack.Screen options= {{headerShown: false}} name="Home" component={TabNavigator} />
+          <Stack.Screen options= {{headerShown: false}} name="Task" component={TaskScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -45,4 +52,45 @@ export default function AppNavigation() {
       </NavigationContainer>
     );
   }
+}
+const TabNavigator = () => {
+  // Schedule Screen
+  // Task Screen
+  // Home Screen 
+  // Progress Screen 
+  // Profile Screen
+  return (
+  <Tab.Navigator initialRouteName='Home'>
+    <Tab.Screen 
+      name='Schedule' 
+      component={ScheduleScreen}
+      options={{headerShown: false, title: 'Schedule', tabBarIcon: ({focused}) => (
+        <Image source={require("../assets/images/schedule.png")} style= {{width: 70, height: 70}} />)}} />
+    
+    <Tab.Screen 
+      name='Task' 
+      component={TaskScreen}
+      options={{headerShown: false, title: 'Tasks', tabBarIcon: ({focused}) => (
+        <Image source={require("../assets/images/todo.png")} style= {{width: 70, height: 70}} />)}} />
+    
+    <Tab.Screen 
+      name='Home' 
+      component={HomeScreen}
+      options={{headerShown: false, title: 'Home', tabBarIcon: ({focused}) => (
+        <Image source={require("../assets/images/home.png")} style= {{width: 70, height: 70}} />)}} />
+
+    <Tab.Screen 
+      name='Progress' 
+      component={ProgressScreen}
+      options={{headerShown: false, title: 'Progress', tabBarIcon: ({focused}) => (
+        <Image source={require("../assets/images/progress.png")} style= {{width: 70, height: 70}} />)}} />
+
+    <Tab.Screen 
+      name='Profile' 
+      component={ProfileScreen}
+      options={{headerShown: false, title: 'Profile', tabBarIcon: ({focused}) => (
+        <Image source={require("../assets/images/profile.png")} style= {{width: 70, height: 70}} />)}} />
+
+  </Tab.Navigator>
+ )
 }
