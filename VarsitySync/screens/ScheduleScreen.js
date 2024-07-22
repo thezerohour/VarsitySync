1e0fa080fb3c8d3cfec8dfe64243206dd53cc3bc
Piraveen Alexander <alex.piraveen@gmail.com>
2024-07-22 19:38:17 +0800

    data fetching finished, struct and event adding WIP

--- VarsitySync/screens/ScheduleScreen.js
+++ VarsitySync/screens/ScheduleScreen.js
@@ -3,128 +3,59 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native'
 import { Calendar, CalendarList, Agenda, calendarTheme } from 'react-native-calendars';
+import { auth, db } from '../firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 
 export default function ScheduleScreen() {
+
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const uid = currentUser.uid;
+        try {
+          const userDocRef = doc(db, 'users', uid, 'schedules', '1');
+          const userDoc = await getDoc(userDocRef);
+          if (userDoc.exists()) {
+            setUserData(userDoc.data());
+            console.log('data fetched');
+          } else {
+            console.log('No such document!');
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+
+    fetchUserData();
+    console.log(userData);
+  });
+
   return (
     <SafeAreaView style={styles.container}>
-      <CalendarList
-        style={{ width: 400, height: 700}}
-        theme={{
-          backgroundColor: '#06213E',
-          calendarBackground: '#06213E',
-          textSectionTitleColor: '#FFFFFF',
-          selectedDayBackgroundColor: '#00adf5',
-          selectedDayTextColor: '#FFFFFF',
-          todayTextColor: '#00adf5',
-          dayTextColor: '#FFFFFF',
-          textDisabledColor: '#FFFFFF',
-          dotColor: '#00adf5',
-          selectedDotColor: '#FFFFFF',
-          arrowColor: '#FFFFFF',
-          disabledArrowColor: '#FFFFFF',
-          monthTextColor: '#FFFFFF',
-          indicatorColor: '#FFFFFF',
-          textDayFontFamily: 'monospace',
-          textMonthFontFamily: 'monospace',
-          textDayHeaderFontFamily: 'monospace',
-          textDayFontWeight: '300',
-          textMonthFontWeight: 'bold',
-          textDayHeaderFontWeight: '300',
-          textDayFontSize: 16,
-          textMonthFontSize: 16,
-          textDayHeaderFontSize: 16
-        }}
-        
-      />
       <Agenda
-  // The list of items that have to be displayed in agenda. If you want to render item as empty date
-  // the value of date key has to be an empty array []. If there exists no value for date key it is
-  // considered that the date in question is not yet loaded
-  items={{
-    '2012-05-22': [{name: 'item 1 - any js object'}],
-    '2012-05-23': [{name: 'item 2 - any js object', height: 80}],
-    '2012-05-24': [],
-    '2012-05-25': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
-  }}
-  // Callback that gets called when items for a certain month should be loaded (month became visible)
-  loadItemsForMonth={month => {
-    console.log('trigger items loading');
-  }}
-  // Callback that fires when the calendar is opened or closed
-  onCalendarToggled={calendarOpened => {
-    console.log(calendarOpened);
-  }}
-  // Callback that gets called on day press
-  onDayPress={day => {
-    console.log('day pressed');
-  }}
-  // Callback that gets called when day changes while scrolling agenda list
-  onDayChange={day => {
-    console.log('day changed');
-  }}
-  // Initially selected day
-  selected={'2012-05-16'}
-  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-  minDate={'2012-05-10'}
-  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-  maxDate={'2012-05-30'}
-  // Max amount of months allowed to scroll to the past. Default = 50
-  pastScrollRange={50}
-  // Max amount of months allowed to scroll to the future. Default = 50
-  futureScrollRange={50}
-  // Specify how each item should be rendered in agenda
-  renderItem={(item, firstItemInDay) => {
-    return <View />;
-  }}
-  // Specify how each date should be rendered. day can be undefined if the item is not first in that day
-  renderDay={(day, item) => {
-    return <View />;
-  }}
-  // Specify how empty date content with no items should be rendered
-  renderEmptyDate={() => {
-    return <View />;
-  }}
-  // Specify how agenda knob should look like
-  renderKnob={() => {
-    return <View />;
-  }}
-  // Specify what should be rendered instead of ActivityIndicator
-  renderEmptyData={() => {
-    return <View />;
-  }}
-  // Specify your item comparison function for increased performance
-  rowHasChanged={(r1, r2) => {
-    return r1.text !== r2.text;
-  }}
-  // Hide knob button. Default = false
-  hideKnob={true}
-  // When `true` and `hideKnob` prop is `false`, the knob will always be visible and the user will be able to drag the knob up and close the calendar. Default = false
-  showClosingKnob={false}
-  // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-  markedDates={{
-    '2012-05-16': {selected: true, marked: true},
-    '2012-05-17': {marked: true},
-    '2012-05-18': {disabled: true}
-  }}
-  // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-  disabledByDefault={true}
-  // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly
-  onRefresh={() => console.log('refreshing...')}
-  // Set this true while waiting for new data from a refresh
-  refreshing={false}
-  // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView
-  refreshControl={null}
-  // Agenda theme
-  theme={{
-    ...calendarTheme,
-    agendaDayTextColor: 'yellow',
-    agendaDayNumColor: 'green',
-    agendaTodayColor: 'red',
-    agendaKnobColor: 'blue'
-  }}
-  // Agenda container style
-  style={{}}
-/>
+        items={{
+          '2024-07-13': [{ name: 'item 1 - any js object' }],
+          '2024-07-14': [{ name: 'item 2 - any js object', height: 80 }],
+          '2024-07-15': [],
+          '2024-07-16': [{ name: 'item 3 - any js object' }, { name: 'any js object' }],
+        }}
+        loadItemsForMonth={(month) => {
+          console.log('trigger items loading')
+        }}
+        selected={'2024-07-13'}
+        renderItem={(item) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          )
+        }}
+      />
     </SafeAreaView>
   )
 }
@@ -132,32 +63,6 @@ export default function ScheduleScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#06213E',
-  },
-  profileImage: {
-    width: 250,
-    height: 250,
-    borderRadius: 20,
-    marginTop:0,
-    marginBottom: -30,
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 5,
-    color:"#FFFFFF",
-  },
-  name: {
-    fontSize: 23,
-    marginBottom: 3,
-    fontWeight:"bold",
-    color:"#FFFFFF",
-  },
-  email: {
-    fontSize: 16,
-    marginBottom: 40,
-    color:"#FFFFFF",
+    backgroundColor: 'white',
   },
 });
